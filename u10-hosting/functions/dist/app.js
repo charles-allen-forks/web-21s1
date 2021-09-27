e01f3899e4850f7e18a650938bb383502237407f
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-09-27 21:44:59 +0700

    [U10] Remove dist

--- u10-hosting/functions/dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-const express = require("express");
-exports.app = express();
-exports.app.get('/', (req, res) => res.send('Hello BC Students from Cloud Functions'));
-exports.app.get('/cheese', (req, res) => res.send('Cheese'));
-//# sourceMappingURL=app.js.map
--- u10-hosting/functions/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.example = void 0;
-const functions = require("firebase-functions");
-const app_1 = require("./app");
-exports.example = functions.https.onRequest(app_1.app);
-//# sourceMappingURL=index.js.map
