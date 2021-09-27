8f5ad3455a01b158884e0a6626321de53bfdc6a9
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-09-27 18:40:30 +0700

    [MP2] Fix lint

--- proj2-clientside-cinema/hosting/.eslintrc.js
+++ proj2-clientside-cinema/hosting/.eslintrc.js
@@ -4,15 +4,32 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/standard',
     '@vue/typescript/recommended'
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.vue'],
+      extends: [
+        'plugin:vue/recommended',
+        '@vue/standard',
+        '@vue/typescript/recommended',
+        'standard-with-typescript'
+      ],
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2021,
+        extraFileExtensions: ['.vue'],
+        parser: '@typescript-eslint/parser',
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
+        tsconfigRootDir: __dirname
+      }
+    }
+  ]
 }
